1ed1ec15a12a2204b8221cc3fc6c9c1240308ca3
Lenugo <lnunez@intellisys.com.do>
2021-09-13 23:43:50 -0400

    config: bg-image, edit margin auto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [".src/**/*.html", ".src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "pattern-bg": "url('/images/pattern-bg.svg')",
+        "pattern-curve": "url('/images/pattern-curve.svg')",
+        "pattern-quotes": "url('/images/pattern-quotes.svg')",
+        pas: "url('/images/image-john.jpg')",
+      },
+    },
   },
   variants: {
     extend: {},
